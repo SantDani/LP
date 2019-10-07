9b666b4bfb58c2279b4f516e0fd3c8f965fc5b7d
Santiago <SantiagoSRP@live.com>
2019-10-08 01:08:14 +0200

    Matriz dinamica acabado

--- T0_RepasoMP/Repaso_MP_Ex6EvaluableMatrizDinamica/Repaso_MP_Ex6EvaluableMatrizDinamica/Matriu.h
+++ T0_RepasoMP/Repaso_MP_Ex6EvaluableMatrizDinamica/Repaso_MP_Ex6EvaluableMatrizDinamica/Matriu.h
@@ -8,7 +8,7 @@ public:
 	Matriu() : m_matriu(nullptr), m_nFiles(0), m_nColumnes(0) {}
 	//Matriu(int nFiles, int nColumnes) : m_matriu(nullptr), m_nFiles(0), m_nColumnes(0) {resize(nFiles, nColumnes);}
 	Matriu(int nFiles, int nCol);
-	Matriu(const Matriu& m) : m_matriu(nullptr), m_nFiles(0), m_nColumnes(0) { *this = m; }
+	Matriu(const Matriu& m);
 	~Matriu();
 
 	void liberarMemoria();
